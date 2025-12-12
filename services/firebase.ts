8ba6c704bab5024359b597c0a48687f6f21db0cd
Mohd Junaid Pasha <mdjunadepasha@gmail.com>
2025-12-12 21:10:13 +0530

    Handle notification listener errors gracefully

--- services/firebase.ts
+++ services/firebase.ts
@@ -124,6 +124,8 @@ export const notifications = {
                     }
                 }
             });
+        }, (error) => {
+            console.error("Notification Listener Error:", error);
         });
     },
 
